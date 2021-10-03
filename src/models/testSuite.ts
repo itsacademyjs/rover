e3c23910b7565e3668097d619477973d3fb2ceb2
Joel E. Rego <joelerego@outlook.com>
2021-10-03 14:41:26 +0530

    feat: create the `testSuite` model

---
+++ src/models/testSuite.ts
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const testSuiteSchema = new Schema(
+    {
+        title: {
+            type: String,
+            maxlength: 1024,
+            trim: true,
+        },
+        description: {
+            type: String,
+            maxlength: 10240,
+            trim: true,
+        },
+        handle: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+        },
+        testCases: {
+            type: [
+                {
+                    title: {
+                        type: String,
+                        maxlength: 1024,
+                        trim: true,
+                    },
+                    description: {
+                        type: String,
+                        maxlength: 10240,
+                        trim: true,
+                    },
+                },
+            ],
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("TestSuite", testSuiteSchema);
